dc09c8fd8ab3040e253fa6ce68d6525326f7860e
RogerKTC10 <ntchamroger82@gmail.com>
2025-12-21 21:15:42 +0100

    FIN

--- APIClient/APIClient/src/main/java/com/RepositoryLiaison/InterfaceUtilisateur.java
+++ APIClient/APIClient/src/main/java/com/RepositoryLiaison/InterfaceUtilisateur.java
@@ -1,8 +1,11 @@
 package com.RepositoryLiaison;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.example.APIClient.Utilisateur;
 
+@Repository
 public interface InterfaceUtilisateur extends JpaRepository<Utilisateur , Long>{
     //IMPLEMENTATION DES METHODES D'ACTION SUR LA BASE DE DONNEES grace à JPA REPOSITORY 
 
