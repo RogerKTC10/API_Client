28c507da76666e8cbfccbe05d8662d66adb85187
RogerKTC10 <ntchamroger82@gmail.com>
2025-12-21 21:01:49 +0100

    ...

--- APIClient/APIClient/src/main/java/com/example/APIClient/ApiClientApplication.java
+++ APIClient/APIClient/src/main/java/com/example/APIClient/ApiClientApplication.java
@@ -2,10 +2,12 @@ package com.example.APIClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; 
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
+@EntityScan(basePackages = "com.example.APIClient") 
+@EnableJpaRepositories(basePackages = "com.RepositoryLiaison")
 public class ApiClientApplication {
 
 	public static void main(String[] args) {
